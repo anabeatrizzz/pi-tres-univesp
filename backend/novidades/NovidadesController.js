4e3783c5f08818ed74fcd28354d7b693b74df59c
JoseAllanAndradedeSantana <allan_santana2810@hotmail.com>
2022-11-09 14:47:23 -0300

    ajustando endpoint e criando endpoint para editar novidade

--- backend/novidades/NovidadesController.js
+++ backend/novidades/NovidadesController.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const novidades = require("./Novidades");
+const { where } = require("sequelize");
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
@@ -14,15 +15,25 @@ app.get("/novidades",(req,res) =>{
     })
 })
 
-app.post("/novidade/create",(req,res) => {
+app.post("/novidades",(req,res) => {
     var titulo = req.body.titulo;
     var descricao = req.body.descricao;
-    user.create({
+    novidades.create({
         titulo: titulo,
         descricao: descricao
     }).then(() =>{
         res.statusCode(201);
     });
-})
+});
+
+app.put("/novidade/:id",(req,res) => {
+   var id = parseInt(req.params.id);
+   var titulo = req.body.titulo;
+   console.log(titulo);
+  novidades.update({titulo:titulo},{where:{id:id}}).then(data => {
+    console.log(data);
+    res.sendStatus(200);
+   });
+});
 
 module.exports = app;
