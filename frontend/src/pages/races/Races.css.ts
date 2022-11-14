4c916df6c97a13f4c019641842ff41e0d5e43fbb
Ana Beatriz <anabeatriz.augusto06@yahoo.com>
2022-11-14 07:34:21 -0300

    add texts and img upload

--- frontend/src/pages/races/Races.css.ts
+++ frontend/src/pages/races/Races.css.ts
@@ -1,8 +1,25 @@
+import BgImg from "../../assets/races-bg.png";
+
 const styles = {
   racesContainer: {
     display: "flex",
+    paddingTop: 10
     //justifyContent: "space-evenly",
     //flexWrap: "wrap" as "wrap"
+  },
+
+  raceOutterContainer: {
+    display: "flex",
+    justifyContent: "space-around",
+    flexDirection: "row" as "row",
+    flexBasis: "50%"
+  },
+
+  raceInnerContainer: {
+    backgroundImage: `url(${BgImg})`,
+    backgroundSize: "100%",
+    backgroundRepeat: "no-repeat",
+    padding: 60
   },
 
   editIconDiv: {
--- frontend/src/pages/races/Races.tsx
+++ frontend/src/pages/races/Races.tsx
@@ -6,9 +6,21 @@ import { Card } from "../characters"
 import styles from "./Races.css";
 import Edit from '@mui/icons-material/Edit';
 import BgImg from "../../assets/races-bg.png";
+import { useDropzone } from "react-dropzone";
+import defaultProfile from "../../assets/user-profile-photo.jpg";
 
 export default function Races() {
   const [count, setCount] = useState(0);
+  const [filePath, setFilePath] = useState('Imagemx.jpg')
+  const { acceptedFiles, getRootProps, getInputProps, open } = useDropzone({
+    accept: {
+      'image/png': ['.png', '.jpg'],
+    },
+    maxFiles: 1,
+    onDrop: (acceptedFiles) => {
+      setFilePath(URL.createObjectURL(acceptedFiles[0]))
+    }
+  });
 
   return (
     <Wrapper paperComponent title="RAÇAS">
@@ -18,14 +30,38 @@ export default function Races() {
         title="Adicionar equipamento"
       />
       <div style={styles.racesContainer}>
-        <div style={{ display: "flex", justifyContent: "space-around", flexDirection: "row"}}>
-          <div style={{ backgroundImage: `url(${BgImg})`, backgroundSize: "100%", backgroundRepeat: "no-repeat" }}><Typography>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel quam lectus. Nulla lobortis sed sem sit amet scelerisque. Vivamus pulvinar pulvinar mi, non ultrices leo ultricies quis. Praesent lacinia dui tempor ante finibus consectetur. Proin sit amet quam laoreet, aliquam neque eget, interdum ligula. Nullam vel ex a dui dignissim viverra. Curabitur dictum elit lectus, sit amet viverra diam sodales in. Praesent rutrum felis at nisl molestie fermentum. Sed eget turpis sit amet est ornare mollis id at diam. Nunc vitae odio molestie, rhoncus nisi vitae, posuere enim. Nullam tristique sapien non tortor hendrerit, ut placerat dui ornare. Duis aliquet, massa eget ultrices aliquet, ante ante gravida mauris, at dignissim leo neque eget velit. Nullam sit amet congue erat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;</Typography></div>
-          <div style={{ backgroundImage: `url(${BgImg})`, backgroundSize: "100%", backgroundRepeat: "no-repeat" }}><Typography>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel quam lectus. Nulla lobortis sed sem sit amet scelerisque. Vivamus pulvinar pulvinar mi, non ultrices leo ultricies quis. Praesent lacinia dui tempor ante finibus consectetur. Proin sit amet quam laoreet, aliquam neque eget, interdum ligula. Nullam vel ex a dui dignissim viverra. Curabitur dictum elit lectus, sit amet viverra diam sodales in. Praesent rutrum felis at nisl molestie fermentum. Sed eget turpis sit amet est ornare mollis id at diam. Nunc vitae odio molestie, rhoncus nisi vitae, posuere enim. Nullam tristique sapien non tortor hendrerit, ut placerat dui ornare. Duis aliquet, massa eget ultrices aliquet, ante ante gravida mauris, at dignissim leo neque eget velit. Nullam sit amet congue erat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;</Typography></div>
+        <div style={styles.raceOutterContainer}>
+          <div style={styles.raceInnerContainer}>
+            <input {...getInputProps()} />
+            <img
+              alt="foto do personagem"
+              src={filePath !== 'Imagemx.jpg' ? filePath : defaultProfile}
+              width="25%"
+              onClick={open}
+            />
+          </div>
+          <div style={styles.raceInnerContainer}>
+            <Typography>Porte fisico medio-grande, minimo de 180cm e max 220cm, todos verdes/amarelados. Completamente imune a venenos. São uma raça de guerreiros bravos e loucos, recebem 01 de vantagem em testes de: Intimidar, Ameaçar, e Determinação.
+              Não muito comuns.
+            </Typography>
+          </div>
         </div>
 
-        <div>
-          <div style={{ backgroundImage: `url(${BgImg})`, backgroundSize: "100%", backgroundRepeat: "no-repeat" }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel quam lectus. Nulla lobortis sed sem sit amet scelerisque. Vivamus pulvinar pulvinar mi, non ultrices leo ultricies quis. Praesent lacinia dui tempor ante finibus consectetur. Proin sit amet quam laoreet, aliquam neque eget, interdum ligula. Nullam vel ex a dui dignissim viverra. Curabitur dictum elit lectus, sit amet viverra diam sodales in. Praesent rutrum felis at nisl molestie fermentum. Sed eget turpis sit amet est ornare mollis id at diam. Nunc vitae odio molestie, rhoncus nisi vitae, posuere enim. Nullam tristique sapien non tortor hendrerit, ut placerat dui ornare. Duis aliquet, massa eget ultrices aliquet, ante ante gravida mauris, at dignissim leo neque eget velit. Nullam sit amet congue erat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;</div>
-          <div style={{ backgroundImage: `url(${BgImg})`, backgroundSize: "100%", backgroundRepeat: "no-repeat" }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel quam lectus. Nulla lobortis sed sem sit amet scelerisque. Vivamus pulvinar pulvinar mi, non ultrices leo ultricies quis. Praesent lacinia dui tempor ante finibus consectetur. Proin sit amet quam laoreet, aliquam neque eget, interdum ligula. Nullam vel ex a dui dignissim viverra. Curabitur dictum elit lectus, sit amet viverra diam sodales in. Praesent rutrum felis at nisl molestie fermentum. Sed eget turpis sit amet est ornare mollis id at diam. Nunc vitae odio molestie, rhoncus nisi vitae, posuere enim. Nullam tristique sapien non tortor hendrerit, ut placerat dui ornare. Duis aliquet, massa eget ultrices aliquet, ante ante gravida mauris, at dignissim leo neque eget velit. Nullam sit amet congue erat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;</div>
+        <div style={styles.raceOutterContainer}>
+          <div style={styles.raceInnerContainer}>
+            <input {...getInputProps()} />
+            <img
+              alt="foto do personagem"
+              src={filePath !== 'Imagemx.jpg' ? filePath : defaultProfile}
+              width="25%"
+              onClick={open}
+            />
+          </div>
+
+          <div style={styles.raceInnerContainer}>
+            <Typography  style={{ fontFamily: "Griffy", fontWeight: 700}}>Porte fisico medio-grande, minimo de 180cm e max 220cm, todos verdes/amarelados.<br /><br />Completamente imune a venenos. São uma raça de guerreiros bravos e loucos, recebem 01 de vantagem em testes de: Intimidar, Ameaçar, e Determinação.<br/><br />Não muito comuns.
+            </Typography>
+          </div>
         </div>
         {/* {
           Array(count).fill(1).map((_, index) => {
