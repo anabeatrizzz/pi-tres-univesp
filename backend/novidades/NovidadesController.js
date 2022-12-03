e5549a27503d24e593472e2183903aab71dabbfc
JoseAllanAndradedeSantana <allan_santana2810@hotmail.com>
2022-12-02 21:13:40 -0300

    add busca novidade

--- backend/novidades/NovidadesController.js
+++ backend/novidades/NovidadesController.js
@@ -19,7 +19,6 @@ app.get("/novidade/:id",(req,res) => {
     if(isNaN(req.params.id)){
         return res.sendStatus(404);
      }
-
      let id = parseInt(req.params.id);
      novidades.findOne({where:{id:id}}).then(novidade => {
         res.json(novidade);
